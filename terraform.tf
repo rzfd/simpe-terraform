96c0842e062f9a3bf01c05cc847e7296058ffc9c
Rizki Fadillah <rizkifadillah783@gmail.com>
2024-11-26 23:16:01 +0700

    feat: add another provider

--- terraform.tf
+++ terraform.tf
@@ -5,5 +5,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.76.0"
     }
+    https = {
+      source = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
+    }
   }
 }
